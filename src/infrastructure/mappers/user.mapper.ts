1698441d5d0e0fc6a4ed01bd5f2f3e355b97ea3c
Reclax <marcoserrano2206@gmail.com>
2025-05-28 12:45:05 -0500

    se corrigue error de roles

--- src/infrastructure/mappers/user.mapper.ts
+++ src/infrastructure/mappers/user.mapper.ts
@@ -1,10 +1,9 @@
-
 import { CustomError, UserEntity } from '../../domain';
 
 export class UserMapper {
   
   static userEntityFromObject(object: { [key: string]:any }) {
-    const { id, _id, name, email, password, roles } = object;
+    const { id, _id, name, email, password } = object;
 
     if (!_id && !id) {
       throw CustomError.badRequest('id no encontrado');
@@ -13,14 +12,13 @@ export class UserMapper {
     if (!name) throw CustomError.badRequest('name no encontrado');
     if (!email) throw CustomError.badRequest('email no encontrado');
     if (!password) throw CustomError.badRequest('password no encontrado');
-    if (!roles) throw CustomError.badRequest('roles no encontrado');
 
     return new UserEntity(
       _id || id,
       name, 
       email,
-      password,
-      roles
+      password
     );
   }
 }
+
